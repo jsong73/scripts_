1e6c9e9217a0dbaf0a396809770b34189373127c
Lex Matthews <tanukiguernica@gmail.com>
2022-10-20 10:15:15 -0400

    Update copy_solutions.sh

--- bashScripts/copy_solutions.sh
+++ bashScripts/copy_solutions.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 CWD=$(basename $PWD)
 TARGETDIR=$(find ../../../GT-VIRT-FSF-FT-09-2022-U-LOLC -maxdepth 1 -name "*${CWD}*")
-echo $CWD
-echo $TARGETDIR
 
-echo "What activity did they start on?"
-read START
-echo "What activity are they stopping on?"
-read STOP
-
-i=$START
+STOP= $2
+i=$1
 
 while [ $i -le $STOP ]
 do
