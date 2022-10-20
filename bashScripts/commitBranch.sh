be0c7c0c5f239ffadb42d0f97aa065d5f233d208
Lex Matthews <tanukiguernica@gmail.com>
2022-10-20 12:20:39 -0400

    Version control scripts updated

--- bashScripts/commitBranch.sh
+++ bashScripts/commitBranch.sh
@@ -3,6 +3,6 @@ read NEW
 BRANCH=$(git name-rev --name-only HEAD)
 
 git add .
-git commit -m $1
-git push origin $BRANCH
-git checkout -b $NEW
+git commit -m "${1}"
+git push origin "${BRANCH}"
+git checkout -b "${NEW}"
